6e2d6c442624b0fd580d78b4d98f00677dfd6a31
Krrish166 <138754394+Krrish166@users.noreply.github.com>
2026-04-03 16:24:49 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = "/api";  // For reverse proxy it is mandatory so dont change
-const API_BASE_URL = "https://52.66.197.4:84"
+const API_BASE_URL = "http://52.66.197.4:84"
 
 // const API_BASE_URL = "http://98.92.112.243:84"
 //const API_BASE_URL = "http://backend.515800584282.realhandsonlabs.net";
